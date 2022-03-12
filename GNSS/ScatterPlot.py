4ebbeaa4fd7785cad5e11277172a9920811f3548
Zihe Hu <huzihe06@gmail.com>
2022-03-12 20:27:02 +0800

    [A]add scatter plot and linechart

---
+++ GNSS/ScatterPlot.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+from matplotlib import pyplot as plt
+import xlrd
+
+filename = 'data analysis.xlsx'
+book_wind = xlrd.open_workbook(filename=filename)
+wind_sheet1 = book_wind.sheets()[7]    
+
+eles = wind_sheet1.col_values(2,1)  # 横坐标  高度角
+snrs = wind_sheet1.col_values(1,1)  # 纵坐标  SNR值
+loss = wind_sheet1.col_values(4,1)
+sats = wind_sheet1.col_values(0,1)
+
+for x,y,l,sat in zip(eles,snrs,loss,sats):
+    s = sat[0]
+    if l == 1:
+        if s == 'G':
+            plt.plot(x,y,marker='^',color='r')
+        elif s == 'R':
+            plt.plot(x,y,marker='s',color='r')
+        elif s == 'E':
+            plt.plot(x,y,marker='d',color='r')
+        elif s == 'J':
+            plt.plot(x,y,marker='o',color='r')
+        elif s == 'C':
+            plt.plot(x,y,marker='p',color='r',markersize=4)
+        else:
+            plt.plot(x,y,'ro')
+    if l == 0:
+        if s == 'G':
+            plt.plot(x,y,marker='^',color='g',markersize=4)
+        elif s == 'R':
+            plt.plot(x,y,marker='s',color='g',markersize=4)
+        elif s == 'E':
+            plt.plot(x,y,marker='d',color='g',markersize=4)
+        elif s == 'J':
+            plt.plot(x,y,marker='o',color='g',markersize=4)
+        elif s == 'C':
+            plt.plot(x,y,marker='p',color='g',markersize=4)
+        else:
+            plt.plot(x,y,'go')
+# plt.scatter(x,y)
+plt.show()
---
+++ GNSS/wuce-linechart.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+from matplotlib import pyplot as plt
+import xlrd
+
+filename = 'data analysis.xlsx'
+book_wind = xlrd.open_workbook(filename=filename)
+wind_sheet1 = book_wind.sheets()[6]    
+# 读取第1行标题
+title = wind_sheet1.row_values(0)
+# 读取第一、三列标题以下的数据 col_values(colx,start_row=0,end_row=none)
+x = wind_sheet1.col_values(0,1)
+y8 = wind_sheet1.col_values(8,1)
+y9 = wind_sheet1.col_values(9,1)
+y10 = wind_sheet1.col_values(10,1)
+
+y13 = wind_sheet1.col_values(13,1)
+y14 = wind_sheet1.col_values(14,1)
+y15 = wind_sheet1.col_values(15,1)
+# print(x)
+ 
+# 绘制曲线图
+plt.title('Delt distance',fontsize=12)
+# plt.plot(x, y, x,y2,x,y3,'g-', label='mean')    # 多数据合并到一个图
+plt.subplot(311)    # 分成三行，每行一列，第一行
+plt.plot(x,y8,'g-', label='x-ecef(m)')
+plt.plot(x,y13, label='spp-x-ecef(m)')
+plt.legend()      # 设置图例位置
+
+plt.subplot(312)
+plt.plot(x,y9,'m-', label='y-ecef(m)')
+plt.plot(x,y14, label='spp-y-ecef(m)')
+plt.legend()
+
+plt.subplot(313)
+plt.plot(x,y10,'r-', label='z-ecef(m)')
+plt.plot(x,y15, label='spp-z-ecef(m)')
+plt.legend()
+
+plt.tight_layout()
+# plt.savefig('mean.jpg')   # 保存图片
+plt.show()
