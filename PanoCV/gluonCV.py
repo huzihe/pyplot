40ffa915a86d15d9c01daa296b622fba40832681
Zihe Hu <huzihe06@gmail.com>
2022-11-22 16:26:58 +0800

    [M]update

--- PanoCV/gluonCV.py
+++ PanoCV/gluonCV.py
@@ -12,13 +12,11 @@ ctx = mx.cpu()
 img = image.imread('./data/cv/cv1.jpg')
 img = test_transform(img,ctx)
 
-
 model1 = gluoncv.model_zoo.get_model('deeplab_resnet101_citys', pretrained=True)
 output = model1.predict(img)
 predict = mx.nd.squeeze(mx.nd.argmax(output, 1)).asnumpy()
 sky = (predict==10)
 print('天空率为:',str(len(predict[sky])/(predict.shape[0]*predict.shape[1])))
-
 
 mask = get_color_pallete(predict, 'citys')
 base = mpimg.imread('./data/cv/cv1.jpg')
@@ -41,3 +39,5 @@ plt.imshow(base)
 plt.imshow(mask,alpha=0.5)
 plt.axis('off')
 plt.savefig('./data/cv/cv1_deeplab_ade.jpg',dpi=250,bbox_inches='tight')
+
+print("it's done!")
